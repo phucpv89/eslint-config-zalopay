14a16557b0d6e642be6586b60b0c1e3582b99642
phucpv <phucpv@vng.com.vn>
2019-11-07 14:13:54 +0700

    Init eslint config for zalopay frontend

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  globals: {
+    window: true,
+    navigator: true,
+    document: true
+  },
+  rules: {
+    "max-len": ["error", { code: 80, tabWidth: 2 }],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "never"
+      }
+    ],
+    "eol-last": ["error", "always"],
+    "no-invalid-this": 0,
+    "react/sort-comp": [
+      1,
+      {
+        order: ["static-methods", "lifecycle", "everything-else", "render"]
+      }
+    ],
+
+    "arrow-parens": ["error", "always"],
+    quotes: ["error", "double"],
+    "react/button-has-type": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/button-has-type": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/extensions": [0, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": [1, { ignore: ["history", "children", "navigation"] }],
+    "react/jsx-curly-brace-presence": ["error", "always"],
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/no-static-element-interactions": 0
+  }
+};
